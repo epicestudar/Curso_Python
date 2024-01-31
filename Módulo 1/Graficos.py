6c0cd82cb1089dbc7b585944513a36231d35111b
epicestudar <viniciusftza@gmail.com>
2024-01-31 16:30:55 -0300

    módulo 1

---
+++ Módulo 1/Graficos.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+
+# Criando um gráfico simples
+x = [1, 2, 3, 4, 5]
+y = [10, 15, 7, 12, 8]
+
+plt.plot(x, y)
+plt.xlabel('X-axis')
+plt.ylabel('Y-axis')
+plt.title('Exemplo de Gráfico')
+plt.show()
---
+++ Módulo 1/Strings.py
@@ -0,0 +1,10 @@
+# Concatenando strings
+primeiro_nome = "João"
+ultimo_nome = "Silva"
+nome_completo = primeiro_nome + " " + ultimo_nome
+
+# Imprimindo o nome completo
+print("Nome completo:", nome_completo)
+
+# Transformando para letras maiúsculas
+print("Nome em maiúsculas:", nome_completo.upper())
