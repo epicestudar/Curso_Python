e77c3b472cfece281d2cb694770360b50736fdde
epicestudar <viniciusftza@gmail.com>
2024-01-31 15:57:27 -0300

    começando no python

---
+++ OperacoesBasicas.py
@@ -0,0 +1,10 @@
+# Definindo variáveis
+nome = "Ana"
+idade = 25
+
+# Imprimindo informações
+print(f"Olá, {nome}! Você tem {idade} anos")
+
+# Realizando operações
+idade_no_futuro = idade + 5
+print(f"Daqui a 5 anos, você terá {idade_no_futuro} anos")
