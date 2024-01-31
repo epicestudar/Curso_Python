fa0fe2b2df683ff1ceabbbab3e3a6d94f52e15f2
epicestudar <viniciusftza@gmail.com>
2024-01-31 16:14:12 -0300

    módulo 1

---
+++ Módulo 1/Functions.py
@@ -0,0 +1,7 @@
+# Definindo uma função
+def saudacao(nome):
+    return f"Olá, {nome}!"
+
+# Chamando a função
+mensagem = saudacao("Carlos")
+print(mensagem)
---
+++ Módulo 1/Loops.py
@@ -0,0 +1,6 @@
+# Criando uma lista
+frutas = ["maçã", "uva", "banana"]
+
+# Iterando sobre a lista
+for fruta in frutas:
+    print(fruta)
