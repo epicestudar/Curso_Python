f843047c6bc625d2d98c2cb1f982345d1b40b788
epicestudar <viniciusftza@gmail.com>
2024-02-14 14:11:06 -0300

    loops

--- Módulo 1/Loops.py
+++ Módulo 1/Loops.py
@@ -4,3 +4,8 @@ frutas = ["maçã", "uva", "banana"]
 # Iterando sobre a lista
 for fruta in frutas:
     print(fruta)
+
+# Loop para saber quais números de 0 a 9 são pares
+for numero in range(10):
+    if numero % 2 == 0:
+        print(numero)
