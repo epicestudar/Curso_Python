5cbc6b687d200fc1da4d991d3d407ce143aee30e
epicestudar <viniciusftza@gmail.com>
2024-06-24 13:05:33 -0300

    a

--- Modulo5/AnaliseDadosStreamlit/app.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-import pandas as pd
-
-df = pd.read_csv('movies.csv')
-
-st.write(df.head())
-
-filtro = st.sidebar.selectbox('Selecione o filtro', df.columns)
--- Modulo5/Streamlit/app.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-st.title('Hello Streamlit')
-st.write('Bem-vindo ao curso de Streamlit!')
